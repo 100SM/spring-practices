1b6e2e9d0506e4c219262ff9c003f3d907c91f16
BIT <BIT@DESKTOP-MTA1CLE>
2022-01-21 11:15:58 +0900

    spring practice #4

--- helloweb03/src/main/java/com/poscoict/helloweb/controller/UserController.java
+++ helloweb03/src/main/java/com/poscoict/helloweb/controller/UserController.java
@@ -28,7 +28,19 @@ public class UserController {
 	@ResponseBody
 	@RequestMapping("/update")
 	public String update(@RequestParam("n") String name) {
+		/*
+		 * 만일 n이라는 이름의 파라미터가 없는 경우 400 bad request 에러가 발생함
+		 */
 		System.out.println(name);
 		return "UserController.update(" + name + ")";
+	}
+
+	@ResponseBody
+	@RequestMapping("/update2")
+	public String update2(@RequestParam(value = "n", required = true, defaultValue = "") String name,
+			@RequestParam(value = "a", required = true, defaultValue = "0") int age) {
+		System.out.println("name : " + name);
+		System.out.println("age : " + age);
+		return "UserController.update2(" + name + ", " + age + ")";
 	}
 }
